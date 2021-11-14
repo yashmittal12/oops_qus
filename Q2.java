04c0ab6db3f305e1cb3232e7ba8a0d7d2f100c6c
yashmittal12 <83124446+yashmittal12@users.noreply.github.com>
2021-11-14 17:38:26 +0530

    Palindrome number

---
+++ Q2.java
@@ -0,0 +1,17 @@
+public class Palindrome {
+public class Q8 {
+    public int pali;
+    Palindrome(int p)
+    Q8(int p)
+    {
+        pali= p;
+    }
+public void Pali_check(){
+    }
+
+    public static void main(String[] args) {
+        Palindrome obj = new Palindrome(Integer.parseInt(args[0]));
+        Q8 obj = new Q8(Integer.parseInt(args[0]));
+        obj.Pali_check();
+    }
+}
